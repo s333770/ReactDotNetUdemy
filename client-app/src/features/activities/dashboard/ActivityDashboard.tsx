11ac509c254fbed7ebdc419e97fabb44cad2130c
s333770 <48990117+s333770@users.noreply.github.com>
2021-02-12 15:52:12 +0100

    Done with some more work

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Grid, GridColumn, List } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import { ActivityDetails } from "../details/ActivityDetails";
+import { ActivityForm } from "../form/ActivityForm";
+import { ActivityList } from "./ActivityList";
+
+interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+}
+
+export const ActivityDashboard: React.FunctionComponent<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  editMode,
+  setEditMode,
+  setSelectedActivity,
+  createActivity,
+  editActivity,
+}) => {
+  return (
+    <div>
+      <Grid>
+        <Grid.Column width={10}>
+          <ActivityList
+            activities={activities}
+            selectActivity={selectActivity}
+          />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          {selectedActivity && !editMode && (
+            <ActivityDetails
+              activity={selectedActivity}
+              setEditMode={setEditMode}
+              setSelectedActivity={setSelectedActivity}
+            />
+          )}
+          {editMode && (
+            <ActivityForm
+              setEditMode={setEditMode}
+              activity={selectedActivity!}
+              createActivity={createActivity}
+              editActivity={editActivity}
+            />
+          )}
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+};
